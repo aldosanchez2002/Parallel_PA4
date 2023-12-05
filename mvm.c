9a340d03047923bc6ac47210f4104da05f12f147
Aldo Sanchez <52171931+aldosanchez2002@users.noreply.github.com>
2023-12-05 09:41:20 -0700

    copied skelleton into mvm

--- mvm.c
+++ mvm.c
@@ -1,94 +1,38 @@
-/* test driver program for 1D matrix-vector multiplication */
-#include "mpi.h"
-#include <stdio.h>
+/* Parallel matrix-vector multiplication with 1D block decomposition
+ * We assume the matrix a and vector x have already been distributed.
+ */
+#include <mpi.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-extern void MatrixVectorMultiply(int n, double *a, double *x, double *y, MPI_Comm comm);
-
-int main (int argc, char *argv[])
+void MatrixVectorMultiply(int n, double *a, double *x, double *y, MPI_Comm comm)
 {
-int   numtasks, taskid;
-int i, j, N, n, nlocal;
-double *a, *x, *y, *ycheck, *alocal, *xlocal, *ylocal;
-int proc;
-double start, end;
+    int i, j;
+    int nlocal;         /* Number of locally stored rows of A */
+    double *xbuf;       /* Buffer that will store entire vector x */
+    int npes, myrank;
+    MPI_Status status;
 
-MPI_Init(&argc, &argv);
-MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
-MPI_Comm_rank(MPI_COMM_WORLD,&taskid);
+    MPI_Comm_size(comm, &npes);
+    MPI_Comm_rank(comm, &myrank);
 
-if (argc != 2) {
-   if (taskid == 0) {
-     fprintf(stderr, "Usage: %s <n>\n", argv[0]);
-     fprintf(stderr, "where n is a multiple of the number of tasks.\n");
-   }
-   MPI_Finalize();
-   exit(0);
-}
+    /* Allocate memory for x buffer */
+    xbuf = (double *) malloc(n*sizeof(double));
+    
+    nlocal = n/npes;
 
-/* Read row/column dimension from command line */
-n = atoi(argv[1]);
+    /* Gather entire vector x on each processor */
+    /********************************************/
 
-N=n*n; //size of matrix n x n
-nlocal = n/numtasks;
+    /********************************************/
 
-if (taskid == 0) {
-  a = (double *) malloc(N*sizeof(double));
-  x = (double *) malloc(n*sizeof(double));
-  y = (double *) malloc(n*sizeof(double));
-  ycheck = (double *) malloc(n*sizeof(double));
-
-  /* Initialize a and x */
-  for (i = 0;i < n;i++) {
-    for (j = 0;j < n; j++)
-      a[i*n +j] = 2*i+j;
-    x[i] = i;
-  }
-
-  //print grid
-  printf("Grid size: %d x %d\n", n, n);
-  for (i = 0;i < n;i++) {
-    for (j = 0;j < n; j++)
-      printf("%f ", a[i*n +j]);
-    printf("\n");
-  }
-}
-
-    alocal = (double *) malloc(n*nlocal*sizeof(double));
-    xlocal = (double *) malloc(nlocal*sizeof(double));
-    ylocal = (double *) malloc(nlocal*sizeof(double));
-
-    /* Distribute a and x in 1D row distribution */
-    /*********************************************/
-    MPI_Scatter(a,n*nlocal,MPI_DOUBLE,alocal,n*nlocal,MPI_DOUBLE,0,MPI_COMM_WORLD);
-    MPI_Scatter(x,nlocal,MPI_DOUBLE,xlocal,nlocal,MPI_DOUBLE,0,MPI_COMM_WORLD);
-    /*********************************************/
-
-    /* Each process calls MatrixVectorMultiply */
-
-    start = MPI_Wtime();
-    MatrixVectorMultiply(n, alocal, xlocal, ylocal, MPI_COMM_WORLD);
-    end = MPI_Wtime();
-
-    /* Gather results back to root process */
-    /***************************************/
-    MPI_Gather(ylocal, nlocal, MPI_DOUBLE,y, nlocal, MPI_DOUBLE,0,MPI_COMM_WORLD);
-    /***************************************/
-
-    /* Check results */
-
-    if (taskid == 0) {
-      for (i = 0; i<n; i++) {
-         ycheck[i] = 0;
-         for (j=0; j<n; j++)
-            ycheck[i] += a[i*n+j]*x[j];
-         if (ycheck[i] != y[i])
-            printf("discrepancy: ycheck[%d]=%f, y[%d]=%f\n", i, ycheck[i], i, y[i]);
-      }
-      printf("Done with mvm, y[%d] = %f\n", n-1, y[n-1]);
-      printf("Parallel matrix vector multiplication time: %f sec\n", end-start);
+    /* Perform local matrix-vector multiplication */
+    for (i=0; i<nlocal; i++) {
+      y[i] = 0;
+      for (j=0; j<n; j++)
+         y[i] += a[i*n+j]*xbuf[j];
     }
+ 
+    free(xbuf);
+} 
 
-    MPI_Finalize();
-
-}
