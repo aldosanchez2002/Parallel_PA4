358d6dc79a6d000bfbc5b12e5e93d0febcf83441
Aldo Sanchez <52171931+aldosanchez2002@users.noreply.github.com>
2023-12-06 19:41:20 -0700

    formated added parenthesis

--- mvm.c
+++ mvm.c
@@ -7,49 +7,52 @@
 
 void MatrixVectorMultiply(int n, double *a, double *x, double *y, MPI_Comm comm)
 {
-    int i, j;
-    int nlocal;         /* Number of locally stored rows of A */
-    double *xbuf;       /* Buffer that will store entire vector x */
-    int npes, myrank;
-    MPI_Status status;
+  int i, j;
+  int nlocal;   /* Number of locally stored rows of A */
+  double *xbuf; /* Buffer that will store entire vector x */
+  int npes, myrank;
+  MPI_Status status;
 
-    printf("In MatrixVectorMultiply\n");
-    //print a
-    for (i = 0; i < n; i++) {
-        printf("a[%d] = %f\n", i, a[i]);
-    }
+  printf("In MatrixVectorMultiply\n");
+  // print a
+  for (i = 0; i < n; i++)
+  {
+    printf("a[%d] = %f\n", i, a[i]);
+  }
 
-    MPI_Comm_size(comm, &npes);
-    MPI_Comm_rank(comm, &myrank);
+  MPI_Comm_size(comm, &npes);
+  MPI_Comm_rank(comm, &myrank);
 
-    /* Allocate memory for x buffer */
-    xbuf = (double *) malloc(n*sizeof(double));
-    
-    nlocal = n/npes;
+  /* Allocate memory for x buffer */
+  xbuf = (double *)malloc(n * sizeof(double));
 
-  //x local
-    for (i=0; i<nlocal; i++) {
-      print("x");
-      printf("x[%d] = %f\n", i, x[i]);
+  nlocal = n / npes;
+
+  // x local
+  for (i = 0; i < nlocal; i++)
+  {
+    print("x");
+    printf("x[%d] = %f\n", i, x[i]);
 
     /* Gather entire vector x on each processor */
     /********************************************/
     MPI_Alltoall(x, 1, MPI_DOUBLE, xbuf, n, MPI_DOUBLE, comm);
     /********************************************/
-    
-  //print x buffer
-    for (i = 0; i < n; i++) {
-        printf("xbuf[%d] = %f\n", i, xbuf[i]);
-    }
 
+    // print x buffer
+    for (i = 0; i < n; i++)
+    {
+      printf("xbuf[%d] = %f\n", i, xbuf[i]);
+    }
 
     /* Perform local matrix-vector multiplication */
-    for (i=0; i<nlocal; i++) {
+    for (i = 0; i < nlocal; i++)
+    {
       y[i] = 0;
-      for (j=0; j<n; j++)
-         y[i] += a[i*n+j]*xbuf[j];
+      for (j = 0; j < n; j++)
+        y[i] += a[i * n + j] * xbuf[j];
     }
- 
-    free(xbuf);
-} 
 
+    free(xbuf);
+  }
+}
